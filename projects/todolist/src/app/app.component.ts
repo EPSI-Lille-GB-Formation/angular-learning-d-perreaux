2af2d66a22d98538e7905921268784ea7d5c90e9
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2023-11-17 16:09:28 +0100

    todoList

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TODOS } from './mock-todo';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-  <h1>Hello World </h1>
+  <h1>Liste des choses à faire</h1>
+
+  <ul *ngFor="let tache of todoList">
+    <li *ngIf="!tache.isCompleted">{{ tache.title }}</li>
+
+  </ul>
   `,
   styles: []
 })
 export class AppComponent {
+
+  todoList = TODOS;
+
+  ngOnInit() {
+    console.table(this.todoList);
+    this.selectTodo(8);
+  }
+
+  selectTodo(id: number) {
+    console.table(this.todoList[id-1]);
+  }
 
 }
